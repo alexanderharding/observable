78e86c68034eaef39c943935c09ee47b89b978f2
Alexander Harding <harding1996@gmail.com>
2026-01-10 18:09:24 -0700

    Added defer observable creation utility

--- common/mod.ts
+++ common/mod.ts
@@ -3,3 +3,4 @@ export { empty } from "./empty.ts";
 export { ReplaySubject, type ReplaySubjectConstructor } from "./replay-subject.ts";
 export { AsyncSubject, type AsyncSubjectConstructor } from "./async-subject.ts";
 export { BehaviorSubject, type BehaviorSubjectConstructor } from "./behavior-subject.ts";
+export { defer } from "./defer.ts";
