9921f4992864d7518608745f89ceef240bc8ce4e
Alexander Harding <harding1996@gmail.com>
2026-04-15 00:12:56 -0600

    Updated @observable/replay-subject documentation to align with standard terminology

--- replay-subject/mod.ts
+++ replay-subject/mod.ts
@@ -15,8 +15,8 @@ export type ReplaySubject<Value = unknown> = Subject<Value>;
 export interface ReplaySubjectConstructor {
   /**
    * Creates and returns an object that acts as a variant of [`Subject`](https://jsr.io/@observable/core/doc/~/Subject) that
-   * [pushes](https://jsr.io/@observable/core#push) the last {@linkcode count} of {@linkcode Value|values} to
-   * [`consumers`](https://jsr.io/@observable/core#consumer) upon [`subscribe`](https://jsr.io/@observable/core/doc/~/Observable.subscribe).
+   * [pushes](https://jsr.io/@observable/core#push) the last of the given {@linkcode count} of {@linkcode Value|values} upon
+   * [`subscribe`](https://jsr.io/@observable/core/doc/~/Observable.subscribe).
    * @example
    * Positive integer count
    * ```ts
