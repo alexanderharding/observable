d59df4a74869b97195994c0ab070710d7d77c7d7
Alexander Harding <harding1996@gmail.com>
2026-04-15 00:12:36 -0600

    Updated @observable/behavior-subject documentation to align with standard terminology

--- behavior-subject/mod.ts
+++ behavior-subject/mod.ts
@@ -12,8 +12,7 @@ export type BehaviorSubject<Value = unknown> = Subject<Value>;
 export interface BehaviorSubjectConstructor {
   /**
    * Creates and returns an object that acts as a [`Subject`](https://jsr.io/@observable/core/doc/~/Subject) that keeps track of its current
-   * {@linkcode Value|value} and [pushes](https://jsr.io/@observable/core#push) it to [`consumers`](https://jsr.io/@observable/core#consumer)
-   * upon [`subscribe`](https://jsr.io/@observable/core/doc/~/Observable.subscribe).
+   * {@linkcode Value|value} and [pushes](https://jsr.io/@observable/core#push) it upon [`subscribe`](https://jsr.io/@observable/core/doc/~/Observable.subscribe).
    * @example
    * ```ts
    * import { BehaviorSubject } from "@observable/behavior-subject";
